fab61d79dc64ca213f371f9efe7e58ebfc532aa6
Luca Rosafalco <luca.rosafalco@gmail.com>
2019-02-03 10:13:24 +0100

    Changed linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import sklearn
 
+#Hello
+
 # Load the data
 oecd_bli = pd.read_csv("oecd_bli_2015.csv", thousands=',')
 gdp_per_capita = pd.read_csv("gdp_per_capita.csv",thousands=',',delimiter='\t',
